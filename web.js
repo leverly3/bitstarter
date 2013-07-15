6306fb72593ecbb2d79c836ef2173d3905e9ab89
leverly3 <leverly3@gmail.com>
2013-07-15 02:56:37 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,20 @@
 var express = require('express');
+var fs = require('fs');
+
+var buffer = new Buffer(30);
+
+fs.readFile('index.html', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data);
+  buffer = data;
+});
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World! 2');
+  response.send(buffer.toString);
 });
 
 var port = process.env.PORT || 5000;
