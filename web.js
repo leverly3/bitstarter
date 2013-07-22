22ff1c2ace2e05dfac61bcfed95115380c89903e
leverly3 <leverly3@gmail.com>
2013-07-22 06:01:36 +0000

    adding files hwk 4

--- web.js
+++ web.js
@@ -1,23 +1,15 @@
 var express = require('express');
 var fs = require('fs');
-
-var buffer = new Buffer(30);
-
-fs.readFile('index.html', 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  console.log(data);
-  buffer = data;
-});
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString('utf-8'));
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
